4180ea59384727738d43927bc222344793ad6a89
Gonzalo Garcia <gonzalogarcia2318@gmail.com>
2018-11-19 13:45:38 -0300

    Graficos de Gastos.

--- SistemaGenerico/SistemaGenerico/Gastos/Gastos.Designer.vb
+++ SistemaGenerico/SistemaGenerico/Gastos/Gastos.Designer.vb
@@ -22,13 +22,18 @@ Partial Class Gastos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Gastos))
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
         Me.Panel17 = New System.Windows.Forms.Panel()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.linearChart = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.Panel7 = New System.Windows.Forms.Panel()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Panel6 = New System.Windows.Forms.Panel()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblRegistros = New System.Windows.Forms.Label()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dtpFechaPie = New System.Windows.Forms.DateTimePicker()
+        Me.pieChart = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.Panel5 = New System.Windows.Forms.Panel()
         Me.btnEditar = New System.Windows.Forms.Button()
         Me.PictureBox5 = New System.Windows.Forms.PictureBox()
@@ -44,8 +49,10 @@ Partial Class Gastos
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Panel17.SuspendLayout()
+        CType(Me.linearChart, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel7.SuspendLayout()
-        Me.Panel6.SuspendLayout()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pieChart, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel5.SuspendLayout()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -56,62 +63,94 @@ Partial Class Gastos
         'Panel17
         '
         Me.Panel17.BackColor = System.Drawing.Color.FromArgb(CType(CType(77, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(100, Byte), Integer))
-        Me.Panel17.Controls.Add(Me.Label2)
+        Me.Panel17.Controls.Add(Me.linearChart)
         Me.Panel17.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Panel17.Location = New System.Drawing.Point(240, 77)
         Me.Panel17.Name = "Panel17"
-        Me.Panel17.Size = New System.Drawing.Size(440, 259)
+        Me.Panel17.Size = New System.Drawing.Size(900, 259)
         Me.Panel17.TabIndex = 56
         '
-        'Label2
+        'linearChart
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
-        Me.Label2.Location = New System.Drawing.Point(137, 101)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(162, 37)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "GRAFICO"
+        ChartArea1.Name = "ChartArea1"
+        Me.linearChart.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.linearChart.Legends.Add(Legend1)
+        Me.linearChart.Location = New System.Drawing.Point(29, 30)
+        Me.linearChart.Name = "linearChart"
+        Me.linearChart.Size = New System.Drawing.Size(815, 208)
+        Me.linearChart.TabIndex = 1
+        Me.linearChart.Text = "Chart1"
         '
         'Panel7
         '
         Me.Panel7.BackColor = System.Drawing.Color.FromArgb(CType(CType(77, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(100, Byte), Integer))
-        Me.Panel7.Controls.Add(Me.Label3)
+        Me.Panel7.Controls.Add(Me.lblRegistros)
+        Me.Panel7.Controls.Add(Me.PictureBox6)
+        Me.Panel7.Controls.Add(Me.Label2)
+        Me.Panel7.Controls.Add(Me.dtpFechaPie)
+        Me.Panel7.Controls.Add(Me.pieChart)
         Me.Panel7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Panel7.Location = New System.Drawing.Point(240, 359)
         Me.Panel7.Name = "Panel7"
         Me.Panel7.Size = New System.Drawing.Size(440, 259)
         Me.Panel7.TabIndex = 57
         '
-        'Label3
+        'lblRegistros
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
-        Me.Label3.Location = New System.Drawing.Point(150, 128)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(162, 37)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "GRAFICO"
+        Me.lblRegistros.AutoSize = True
+        Me.lblRegistros.BackColor = System.Drawing.Color.FromArgb(CType(CType(77, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.lblRegistros.CausesValidation = False
+        Me.lblRegistros.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRegistros.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblRegistros.Location = New System.Drawing.Point(95, 139)
+        Me.lblRegistros.Name = "lblRegistros"
+        Me.lblRegistros.Size = New System.Drawing.Size(267, 23)
+        Me.lblRegistros.TabIndex = 800
+        Me.lblRegistros.Text = "No hay registros para ese periodo"
+        Me.lblRegistros.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblRegistros.Visible = False
         '
-        'Panel6
+        'PictureBox6
         '
-        Me.Panel6.BackColor = System.Drawing.Color.FromArgb(CType(CType(77, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(100, Byte), Integer))
-        Me.Panel6.Controls.Add(Me.Label4)
-        Me.Panel6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Panel6.Location = New System.Drawing.Point(701, 359)
-        Me.Panel6.Name = "Panel6"
-        Me.Panel6.Size = New System.Drawing.Size(440, 259)
-        Me.Panel6.TabIndex = 55
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(21, 5)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(17, 17)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 799
+        Me.PictureBox6.TabStop = False
         '
-        'Label4
+        'Label2
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
-        Me.Label4.Location = New System.Drawing.Point(163, 141)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(162, 37)
-        Me.Label4.TabIndex = 2
-        Me.Label4.Text = "GRAFICO"
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(42, 4)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(128, 20)
+        Me.Label2.TabIndex = 798
+        Me.Label2.Text = "Gastos del Periodo"
+        '
+        'dtpFechaPie
+        '
+        Me.dtpFechaPie.Font = New System.Drawing.Font("Arial Narrow", 12.0!)
+        Me.dtpFechaPie.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpFechaPie.Location = New System.Drawing.Point(21, 26)
+        Me.dtpFechaPie.Name = "dtpFechaPie"
+        Me.dtpFechaPie.Size = New System.Drawing.Size(183, 26)
+        Me.dtpFechaPie.TabIndex = 797
+        '
+        'pieChart
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.pieChart.ChartAreas.Add(ChartArea2)
+        Me.pieChart.Location = New System.Drawing.Point(57, 65)
+        Me.pieChart.Name = "pieChart"
+        Me.pieChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen
+        Me.pieChart.Size = New System.Drawing.Size(338, 183)
+        Me.pieChart.TabIndex = 2
+        Me.pieChart.Text = "Gastos "
         '
         'Panel5
         '
@@ -130,7 +169,7 @@ Partial Class Gastos
         Me.Panel5.Controls.Add(Me.PictureBox2)
         Me.Panel5.Controls.Add(Me.Label6)
         Me.Panel5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Panel5.Location = New System.Drawing.Point(700, 77)
+        Me.Panel5.Location = New System.Drawing.Point(700, 359)
         Me.Panel5.Name = "Panel5"
         Me.Panel5.Size = New System.Drawing.Size(440, 259)
         Me.Panel5.TabIndex = 54
@@ -144,7 +183,7 @@ Partial Class Gastos
         Me.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnEditar.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnEditar.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.btnEditar.Location = New System.Drawing.Point(115, 197)
+        Me.btnEditar.Location = New System.Drawing.Point(111, 194)
         Me.btnEditar.Name = "btnEditar"
         Me.btnEditar.Size = New System.Drawing.Size(196, 35)
         Me.btnEditar.TabIndex = 797
@@ -154,7 +193,7 @@ Partial Class Gastos
         'PictureBox5
         '
         Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
-        Me.PictureBox5.Location = New System.Drawing.Point(192, 121)
+        Me.PictureBox5.Location = New System.Drawing.Point(188, 118)
         Me.PictureBox5.Name = "PictureBox5"
         Me.PictureBox5.Size = New System.Drawing.Size(17, 17)
         Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -166,7 +205,7 @@ Partial Class Gastos
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label8.Location = New System.Drawing.Point(213, 120)
+        Me.Label8.Location = New System.Drawing.Point(209, 117)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(46, 20)
         Me.Label8.TabIndex = 795
@@ -176,9 +215,9 @@ Partial Class Gastos
         '
         Me.dtpFecha.Font = New System.Drawing.Font("Arial Narrow", 12.0!)
         Me.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.dtpFecha.Location = New System.Drawing.Point(192, 142)
+        Me.dtpFecha.Location = New System.Drawing.Point(188, 139)
         Me.dtpFecha.Name = "dtpFecha"
-        Me.dtpFecha.Size = New System.Drawing.Size(161, 26)
+        Me.dtpFecha.Size = New System.Drawing.Size(179, 26)
         Me.dtpFecha.TabIndex = 794
         '
         'btnAgregarGasto
@@ -194,7 +233,7 @@ Partial Class Gastos
         Me.btnAgregarGasto.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnAgregarGasto.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnAgregarGasto.ForeColor = System.Drawing.Color.Transparent
-        Me.btnAgregarGasto.Location = New System.Drawing.Point(359, 85)
+        Me.btnAgregarGasto.Location = New System.Drawing.Point(373, 82)
         Me.btnAgregarGasto.Name = "btnAgregarGasto"
         Me.btnAgregarGasto.Size = New System.Drawing.Size(25, 25)
         Me.btnAgregarGasto.TabIndex = 793
@@ -203,7 +242,7 @@ Partial Class Gastos
         'PictureBox4
         '
         Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
-        Me.PictureBox4.Location = New System.Drawing.Point(64, 120)
+        Me.PictureBox4.Location = New System.Drawing.Point(60, 117)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(17, 17)
         Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -215,7 +254,7 @@ Partial Class Gastos
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label7.Location = New System.Drawing.Point(82, 119)
+        Me.Label7.Location = New System.Drawing.Point(78, 116)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(48, 20)
         Me.Label7.TabIndex = 791
@@ -224,7 +263,7 @@ Partial Class Gastos
         'txtMonto
         '
         Me.txtMonto.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtMonto.Location = New System.Drawing.Point(64, 142)
+        Me.txtMonto.Location = New System.Drawing.Point(60, 139)
         Me.txtMonto.Name = "txtMonto"
         Me.txtMonto.Size = New System.Drawing.Size(109, 26)
         Me.txtMonto.TabIndex = 790
@@ -232,7 +271,7 @@ Partial Class Gastos
         'PictureBox3
         '
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(62, 61)
+        Me.PictureBox3.Location = New System.Drawing.Point(58, 58)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(20, 20)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -244,9 +283,9 @@ Partial Class Gastos
         Me.cboGastos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cboGastos.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cboGastos.FormattingEnabled = True
-        Me.cboGastos.Location = New System.Drawing.Point(64, 84)
+        Me.cboGastos.Location = New System.Drawing.Point(60, 81)
         Me.cboGastos.Name = "cboGastos"
-        Me.cboGastos.Size = New System.Drawing.Size(289, 28)
+        Me.cboGastos.Size = New System.Drawing.Size(307, 28)
         Me.cboGastos.TabIndex = 787
         '
         'Label5
@@ -254,7 +293,7 @@ Partial Class Gastos
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label5.Location = New System.Drawing.Point(88, 61)
+        Me.Label5.Location = New System.Drawing.Point(84, 58)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(53, 20)
         Me.Label5.TabIndex = 786
@@ -291,20 +330,18 @@ Partial Class Gastos
         Me.ClientSize = New System.Drawing.Size(1152, 640)
         Me.Controls.Add(Me.Panel17)
         Me.Controls.Add(Me.Panel7)
-        Me.Controls.Add(Me.Panel6)
         Me.Controls.Add(Me.Panel5)
         Me.Name = "Gastos"
         Me.Text = "Gastosvb"
         Me.Controls.SetChildIndex(Me.Panel5, 0)
-        Me.Controls.SetChildIndex(Me.Panel6, 0)
         Me.Controls.SetChildIndex(Me.Panel7, 0)
         Me.Controls.SetChildIndex(Me.Panel17, 0)
         Me.Panel17.ResumeLayout(False)
-        Me.Panel17.PerformLayout()
+        CType(Me.linearChart, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel7.ResumeLayout(False)
         Me.Panel7.PerformLayout()
-        Me.Panel6.ResumeLayout(False)
-        Me.Panel6.PerformLayout()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pieChart, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel5.ResumeLayout(False)
         Me.Panel5.PerformLayout()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
@@ -316,11 +353,7 @@ Partial Class Gastos
     End Sub
     Friend WithEvents Panel17 As System.Windows.Forms.Panel
     Friend WithEvents Panel7 As System.Windows.Forms.Panel
-    Friend WithEvents Panel6 As System.Windows.Forms.Panel
     Friend WithEvents Panel5 As System.Windows.Forms.Panel
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents cboGastos As System.Windows.Forms.ComboBox
@@ -334,4 +367,10 @@ Partial Class Gastos
     Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
     Friend WithEvents Label8 As System.Windows.Forms.Label
     Friend WithEvents btnEditar As System.Windows.Forms.Button
+    Friend WithEvents linearChart As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents pieChart As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents dtpFechaPie As System.Windows.Forms.DateTimePicker
+    Friend WithEvents lblRegistros As System.Windows.Forms.Label
 End Class
--- SistemaGenerico/SistemaGenerico/Gastos/Gastos.vb
+++ SistemaGenerico/SistemaGenerico/Gastos/Gastos.vb
@@ -2,7 +2,7 @@
 Public Class Gastos
     Inherits MenuBase
 
-    Dim rs As OdbcDataReader
+    Dim rs, rsRegistros As OdbcDataReader
     Dim sql As String
 
     Private Sub Gastosvb_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
@@ -12,6 +12,9 @@ Public Class Gastos
 
         dtpFecha.ShowUpDown = True
         dtpFecha.CustomFormat = "MMMMyyyy"
+
+        dtpFechaPie.ShowUpDown = True
+        dtpFechaPie.CustomFormat = "MMMMyyyy"
 
         cargarTiposGastos()
         cargarGraficos()
@@ -43,6 +46,73 @@ Public Class Gastos
     End Sub
 
     Private Sub cargarGraficos()
+        'sql = "select descripcion, monto, mes, año anio from gastos g, registrogastos r where r.idgasto = g.idgasto"
+        'Funciones.llenarGrilla(sql, dgvGrilla)
+
+        cargarGraficoLineal()
+        cargarGraficoPie()
+
+
+    End Sub
+
+    Private Sub cargarGraficoLineal()
+        linearChart.Series.Clear()
+
+        linearChart.ChartAreas(0).AxisX.MajorGrid.LineDashStyle = DataVisualization.Charting.ChartDashStyle.Dot
+        linearChart.ChartAreas(0).AxisY.MajorGrid.LineDashStyle = DataVisualization.Charting.ChartDashStyle.Dot
+        linearChart.ChartAreas(0).AxisX.IsMarginVisible = False
+        linearChart.ChartAreas(0).AxisY.IsMarginVisible = False
+
+        ' Elegir inclinacion de los labels
+        'linearChart.ChartAreas(0).AxisX.LabelStyle.Angle = -40
+
+        sql = "select descripcion, idgasto from gastos"
+        rs = Funciones.consulta(sql)
+
+        Dim serieIndex As Integer = 0
+
+        Do While rs.Read
+            linearChart.Series.Add(rs(0)).ChartType = DataVisualization.Charting.SeriesChartType.Spline
+
+            sql = "select monto, año, mes from registrogastos where idgasto = " & rs(1) & " order by fecha"
+            rsRegistros = Funciones.consulta(sql)
+
+
+            Do While rsRegistros.Read
+
+                linearChart.Series(serieIndex).Points.AddXY(abreviarMes(rsRegistros(2)) & " - " & rsRegistros(1), rsRegistros(0))
+
+            Loop
+            serieIndex = serieIndex + 1
+        Loop
+
+
+
+
+        'linearChart.Series(0).Points.AddXY("Enero", 100)
+        'linearChart.Series(0).Points.AddXY("Febrero", 200)
+        'linearChart.Series(0).Points.AddXY("Marzo", 300)
+
+    End Sub
+
+    Private Sub cargarGraficoPie()
+        pieChart.Series.Clear()
+        pieChart.Series.Add("Serie 1").ChartType = DataVisualization.Charting.SeriesChartType.Pie
+
+        sql = "select descripcion, monto from registrogastos r, gastos g where r.idgasto = g.idgasto and mes = '" & DateAndTime.MonthName(dtpFechaPie.Value.Month) & "' and año = " & dtpFechaPie.Value.Year & " "
+        rs = Funciones.consulta(sql)
+
+        Dim hayRegistros As Boolean = False
+        Do While rs.Read
+            hayRegistros = True
+            pieChart.Visible = True
+            lblRegistros.Visible = False
+            pieChart.Series(0).Points.AddXY(" " & rs(0) & " - $ " & rs(1), rs(1))
+        Loop
+        If Not hayRegistros Then
+            pieChart.Visible = False
+            lblRegistros.Visible = True
+        End If
 
     End Sub
 
@@ -52,6 +122,8 @@ Public Class Gastos
         If cboGastos.Text = "" Or txtMonto.Text = "" Then
             MsgBox("Complete todos los campos.", MsgBoxStyle.Critical)
         Else
+
+
             sql = "select idgasto from gastos where descripcion = '" & cboGastos.Text & "'"
             rs = Funciones.consulta(sql)
             Dim idGasto As Integer
@@ -60,13 +132,28 @@ Public Class Gastos
 
                 idGasto = rs(0)
 
-                sql = "insert into registrogastos values ('', " & idGasto & ", " & txtMonto.Text & ", " & dtpFecha.Value.Year & ", '" & DateAndTime.MonthName(dtpFecha.Value.Month) & "')"
-                Funciones.consulta(sql)
 
-                MsgBox("Gasto registrado", MsgBoxStyle.Information)
+                sql = "select count(*) from registrogastos where idgasto = " & idGasto & " and mes = '" & DateAndTime.MonthName(dtpFecha.Value.Month) & "' and año = " & dtpFecha.Value.Year & ""
+                rs = Funciones.consulta(sql)
 
-                cargarGraficos()
+                If rs.Read Then
+                    If rs(0) > 0 Then
+                        MsgBox("El gasto ya esta registrado", MsgBoxStyle.Critical)
+                    Else
+                        sql = "insert into registrogastos values ('', " & idGasto & ", " & txtMonto.Text & ", " & dtpFecha.Value.Year & ", '" & DateAndTime.MonthName(dtpFecha.Value.Month) & "', '" & Format(dtpFecha.Value.Date, "yyyy/MM/dd") & "')"
+                        Funciones.consulta(sql)
 
+                        txtMonto.Clear()
+
+                        MsgBox("Gasto registrado", MsgBoxStyle.Information)
+
+                        cargarGraficos()
+
+                    End If
+                End If
+
+
+            
             End If
         End If
 
@@ -75,4 +162,49 @@ Public Class Gastos
 
     End Sub
 
+    Private Function abreviarMes(ByVal mes As String)
+        Dim mesAbreviado As String = ""
+
+        If mes = "enero" Then
+            mesAbreviado = "ENE"
+        End If
+        If mes = "febrero" Then
+            mesAbreviado = "FEB"
+        End If
+        If mes = "marzo" Then
+            mesAbreviado = "MAR"
+        End If
+        If mes = "abril" Then
+            mesAbreviado = "ABR"
+        End If
+        If mes = "mayo" Then
+            mesAbreviado = "MAY"
+        End If
+        If mes = "junio" Then
+            mesAbreviado = "JUN"
+        End If
+        If mes = "julio" Then
+            mesAbreviado = "JUL"
+        End If
+        If mes = "agosto" Then
+            mesAbreviado = "AGO"
+        End If
+        If mes = "septiembre" Then
+            mesAbreviado = "SEP"
+        End If
+        If mes = "octubre" Then
+            mesAbreviado = "OCT"
+        End If
+        If mes = "noviembre" Then
+            mesAbreviado = "NOV"
+        End If
+        If mes = "diciembre" Then
+            mesAbreviado = "DIC"
+        End If
+        Return mesAbreviado
+    End Function
+
+    Private Sub dtpFechaPie_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpFechaPie.ValueChanged
+        cargarGraficoPie()
+    End Sub
 End Class
